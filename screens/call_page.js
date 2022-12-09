f147fc586402671a2948ffb9cf5e16ba22011830
codewithdarkwa <jnrdrkw1@gmail.com>
2022-12-09 06:24:52 +0000

    update

--- screens/call_page.js
+++ screens/call_page.js
@@ -3,7 +3,6 @@ import ZegoUIKitPrebuiltCall, {GROUP_VOICE_CALL_CONFIG} from '@zegocloud/zego-ui
 import { useNavigation } from '@react-navigation/native';
 
 export default function CallPage() {
-  randomUserID = String(Math.floor(Math.random() * 100000))
 
 const navigation = useNavigation();
   return (
@@ -12,7 +11,7 @@ const navigation = useNavigation();
             appID={1638232801}
             appSign='a9ccfd452c3e48c960255681a4a7c01e6f5aeedadb8330b92c999849da22acfc'
             userID={"codewithdarkwa"}
-            userName={'user_'+randomUserID}
+            userName={'darkwa_01'}
 
             config={{
               ...GROUP_VOICE_CALL_CONFIG,
